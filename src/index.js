eb3cc44a3b03404d2bb6404e8d1fc626fc191761
kiyani <bamba.kiyani@gmail.com>
2020-06-09 21:13:03 -0400

    conditionning rendering component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-    return <div> Hi there! </div>
+class App extends React.Component {
+    constructor(props){
+        super(props);
 
+        // THIS IS THE ONLY TIME we do direct assignment to this.state
+        
+        // initializing our state. On sait que lat est un nombre donc on initialise avec null
+        
+        this.state = {lat: null, errorMessage:'' }; 
+
+        
+        // l'on place cette methode ici et non a l'interieur de render car, si on l,a laissé a l'interieur de render a chaque fois notre browser sera entrant de call la fonction. And we don't need to fetch it multiple time
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude})  
+            },
+            (err) => {
+            this.setState({errorMessage: err.message})
+            }
+            ); 
+    }
+    render(){
+                if (this.state.errorMessage && !this.state.lat){
+                    return <div> Error: {this.state.errorMessage} </div> 
+                }
+                if (!this.state.errorMessage && this.state.lat){
+                    return   <div> Latitude: {this.state.lat} </div> 
+                } else {
+                    return <div> Loading </div>   
+                }
+        }
 }
 
 ReactDom.render(<App/>, document.querySelector('#root'))
