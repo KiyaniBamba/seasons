d21e1811377f031d03a4722798abb0aa57fba244
kiyani <bamba.kiyani@gmail.com>
2020-06-18 17:16:28 -0400

    add Spinner and more

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
+const seasonConfig= {
+    summer: {
+        text: 'Lets hit the beach',
+        iconName:'sun'
+    },
+    winter: {
+        text: 'Burr, it is chilly',
+        iconName:'snowflake'  
+    }
+}
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
          return lat > 0 ? 'summer' : 'winter';
@@ -10,9 +21,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+    // const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+    // const icon = season === 'winter' ? 'snowflake' : 'sun';
+    const {text, iconName} = seasonConfig[season]
+
     return (
-    <div> {text} </div> 
+    <div className={`season-display ${season}`} >
+        <i className={` icon-left massive ${iconName} icon`}/>
+        <h1> {text} </h1>
+        <i className={` icon-right massive ${iconName} icon`}/>
+    </div> 
     )
 };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './SeasonDisplay.css';
 import SeasonDisplay  from "./SeasonDisplay";
+import Spinner from './Spinner.js'
 
 class App extends React.Component {
 
     state = {lat: null, errorMessage:'' };         
     
     componentDidMount(){
-        // l'on place cette methode ici et non a l'interieur de render car, si on l,a laissé a l'interieur de render a chaque fois notre browser sera entrant de call la fonction. And we don't need to fetch it multiple time
+        // l'on place cette methode ici et non a l'interieur de render car, si on l','a laisse a l'interieur de render a chaque fois que notre browser sera entrant de call la fonction. And we don't need to fetch it multiple time
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({ lat: position.coords.latitude}),
             (err) => {
@@ -19,12 +21,12 @@ class App extends React.Component {
     
     render(){
                 if (this.state.errorMessage && !this.state.lat){
-                    return <div> Error: {this.state.errorMessage} </div> 
+                    return <div className={`season-display`} > Error: {this.state.errorMessage} </div> 
                 }
                 if (!this.state.errorMessage && this.state.lat){
                     return   <SeasonDisplay  lat= {this.state.lat} /> 
                 } else {
-                    return <div> Loading </div>   
+                    return <Spinner />   
                 }
         }
 }
